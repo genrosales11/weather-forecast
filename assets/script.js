cfa2948bc536f99e7ce6423dca421290f4c9cbca
genrosales11-minjcA <genrosales11@gmail.com>
2022-07-06 19:31:29 -0700

    link css n js

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+// var weatherApiKey = "fc4bfe1c83b6281b19c2c71d6b57c92c";
+// var weatherURL = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=minutely,hourly&appid=${weatherApiKey}`;
+
+// function fetchWeather(location) {
+//   var { lat, lon } = location;
+//   var apiURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${weatherApiKey}`;
+//   console.log(apiURL)
+//   fetch(apiURL)
+//     .then(function (res) {
+//       return res.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     });
+//}
+
+// var location ={
+//     lat: 27.2046,
+//     lon: 27.2046,
+//}
+// fetchWeather(location);
+
+// https://api.openweathermap.org/data/3.0/onecall?lat=27.2046&lon=27.2046&exclude=minutely,hourly&appid=fc4bfe1c83b6281b19c2c71d6b57c92c
+
+// https://api.openweathermap.org/data/2.5/weather?lat=37.7749&lon=122.4194&appid=fc4bfe1c83b6281b19c2c71d6b57c92c
+
